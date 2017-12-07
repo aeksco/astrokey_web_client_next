86c115549dba38e1b265a835f5dea68a76cb28a7
Alexander Schwartzberg <aeksco@gmail.com>
2017-12-07 16:12:08 -0500

    USB store - read/write macro

--- src/store/usb/chrome_web_usb_service.js
+++ src/store/usb/chrome_web_usb_service.js
@@ -1,9 +1,16 @@
 import _ from 'lodash'
-import { REQUEST_DEVICE_FILTERS, READ_MACRO_CONTROL_TRANSFER } from './constants'
+import { REQUEST_DEVICE_FILTERS, READ_MACRO_CONTROL_TRANSFER, WRITE_MACRO_CONTROL_TRANSFER } from './constants'
 
 // ChromeWebUsbService class definition
+// Responsible for managing USB devices
+// - requesting permission to pair with devices
+// - fetching paired devices
+// - writing data to a device
+// - reading data from a device
+// - write firmware to a device (TODO)
 class ChromeWebUsbService {
   // constructor
+  // TODO - is this needed?
   constructor (options) {
     // this.options = options
     // this.messages = options.messages // TODO - support default messages
@@ -84,21 +91,43 @@ class ChromeWebUsbService {
       let READ_MACRO_OPTIONS = _.clone(READ_MACRO_CONTROL_TRANSFER)
       READ_MACRO_OPTIONS.value = keyIndex
 
-      console.log(READ_MACRO_OPTIONS)
-
-      // NOTE - `device.controlTransferIn` (READS DATA FROM DEVICE)
+      // NOTE - `device.controlTransferIn` READS DATA FROM DEVICE
       // TODO - '256' should be '128'
       // TODO - `256` should be moved into constants.js
       // QUESTION - what is this `256` again, expected return length?
       return deviceInstance.controlTransferIn(READ_MACRO_OPTIONS, 256)
       .then((response) => {
-        // console.log('readMacro response:');
-        // console.log(response);
+        // console.log('readMacro response:')
+        // console.log(new Uint8Array(response.data.buffer))
         return resolve(new Uint8Array(response.data.buffer))
       })
       .catch((err) => {
-        // console.log('readMacro error:')
-        // console.log(err)
+        console.log('readMacro error:')
+        return reject(err)
+      })
+    })
+  }
+
+  // writeMacro
+  // Writes a raw macro to an opened device
+  writeMacro ({ commit }, deviceInstance, keyIndex, data) {
+    // keyIndex in hex: `0x0000`
+    // Returns a Promise to manage asynchonous behavior
+    return new Promise((resolve, reject) => {
+      // Clones the READ_MACRO_CONTROL_TRANSFER request object
+      // And adds custom `value` attribute to handle the index of the key we're reading from
+      let WRITE_MACRO_OPTIONS = _.clone(WRITE_MACRO_CONTROL_TRANSFER)
+      WRITE_MACRO_OPTIONS.value = keyIndex
+
+      // NOTE - `device.controlTransferOut` WRITES DATA TO DEVICE
+      return deviceInstance.controlTransferOut(WRITE_MACRO_OPTIONS, new Uint8Array(data).buffer)
+      .then((response) => {
+        // console.log('writeMacro response:')
+        // console.log(response)
+        return resolve(new Uint8Array(response.data.buffer))
+      })
+      .catch((err) => {
+        console.log('writeMacro error:')
         return reject(err)
       })
     })
@@ -108,92 +137,3 @@ class ChromeWebUsbService {
 
 // TODO - this should export the constructor, rather than an instance
 export default new ChromeWebUsbService()
-
-// // /*
-//  * decaffeinate suggestions:
-//  * DS102: Remove unnecessary code created because of implicit returns
-//  * DS206: Consider reworking classes to avoid initClass
-//  * DS207: Consider shorter variations of null checks
-//  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
-//  */
-// // Filters used to query WebUSB devices
-// // TODO - update filters to query devices by AstroKey VendorID
-// const requestDeviceFilters = [
-//   { vendorId: 0x10c4 }
-// ];
-
-// // # # #
-
-// // ChromeWebUsbService class definition
-// // Responsible for managing USB devices
-// // - fetch all devices
-// // - writing data to a device
-// // - reading data from a device
-// // - write firmware to a device
-// class ChromeWebUsbService extends Marionette.Service {
-//   static initClass() {
-
-//   // readMacro
-//   readMacro(macroIndex) {
-
-//     // Returns a Promise to manage asynchonous behavior
-//     if (macroIndex == null) { macroIndex = 0x0000; }
-//     return new Promise((resolve, reject) => {
-
-//       // TODO - move to constants
-//       const READ_MACRO_CONTROL_TRANSFER = {
-//         'requestType':  'vendor',
-//         'recipient':    'device',
-//         'request':      0x03,
-//         'value':        macroIndex,
-//         'index':        0x02
-//       };
-
-//       // device.controlTransferIn (READS DATA FROM DEVICE)
-//       // TODO - abstract the controlTransferIn request object into a constant (cloned each time)
-//       return d.controlTransferIn(READ_MACRO_CONTROL_TRANSFER, 256) // TODO - '256' should be '128'
-//       .then( response => {
-//         console.log('readMacro response:');
-//         console.log(response);
-//         return resolve(new Uint8Array(response.data.buffer));
-//       })
-//       .catch( err => {
-//         console.log('readMacro error:');
-//         console.log(err);
-//         return reject(err);
-//       });
-//     });
-//   }
-
-//   // writeMacro
-//   writeMacro(macroIndex, data) {
-
-//     // Returns a Promise to manage asynchonous behavior
-//     return new Promise((resolve, reject) => {
-
-//       // wIndex - Request type (0x01 for set macro)
-//       // wValue - Macro index (0 - 4 inclusive)
-//       // bRequest - 3 (hardcoded)
-//       // wLength - number of bytes (should be macro length * 2)
-//       const requestObj = {
-//           'requestType':  'vendor',
-//           'recipient':    'device',
-//           'request':      0x03, // TODO - document
-//           'value':        macroIndex,
-//           'index':        0x01 // TODO - We can use index for the key the macro corresponds to (low-byte = key, high-byte = number of actions in the macro)
-//         };
-
-//       console.log(requestObj);
-
-//       return d.controlTransferOut(requestObj, new Uint8Array(data).buffer)
-//       .then( response => {
-//         console.log(response);
-//         return resolve(response);
-//       })
-//       .catch( err => {
-//         console.log('ERROR SENDING MACRO');
-//         return reject(err);
-//       });
-//     });
-//   }
-// }
