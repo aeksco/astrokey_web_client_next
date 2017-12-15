0f4ed59cc9627c7322a00af7e1a83b2ad002f74b
Alexander Schwartzberg <aeksco@gmail.com>
2017-12-15 11:17:46 -0500

    Firefox WebUSB service

--- src/store/usb/actions.js
+++ src/store/usb/actions.js
@@ -1,13 +1,12 @@
 import ChromeWebUsbService from './chrome_web_usb_service'
+import FirefoxWebUSBService from './firefox_web_usb_service'
 
+// Handles Chrome & Firefox WebUSB service classes
 let WebUsbService
-
 if (window.chrome) {
   WebUsbService = new ChromeWebUsbService()
 } else {
-  // TODO - this implement FirefoxWebUSBService
-  // WebUsbService = new FirefoxWebUSBService()
-  WebUsbService = {}
+  WebUsbService = new FirefoxWebUSBService()
 }
 
 // actions
