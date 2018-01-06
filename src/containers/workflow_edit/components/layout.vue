b466b7b582ff00d6b441258f353bbf45cc2ba4e9
Alexander Schwartzberg <aeksco@gmail.com>
2018-01-06 16:34:57 -0500

    MacroStep editor - styling, draggable steps

--- src/containers/workflow_edit/components/layout.vue
+++ src/containers/workflow_edit/components/layout.vue
@@ -50,26 +50,48 @@
                 <div class="col-lg-12" v-if="editing.type === 'MACRO'">
                   <p class="lead">MACRO</p>
 
+                  <!-- MacroStep Editor -->
                   <div class="row">
                     <div class="col-lg-12">
-                      <ul v-for="macro in editing.value" v-bind:key="macro.order" class='list-unstyled'>
-                        <li class="lead">
-                          {{macro.key}}
 
-                          <span class="fa-stack fa-lg" @click="cycleMacroKeyPosition(macro)">
-                            <i class="fa fa-circle-thin fa-stack-2x"></i>
-                            <i class="fa fa-stack-1x fa-stack">
-                              <i class="fa fa-circle-thin fa-stack-2x fa-2x"></i>
-                              <i class="fa fa-stack-1x fa-long-arrow-down text-warning" v-if="macro.position === 1"></i>
-                              <i class="fa fa-stack-1x fa-long-arrow-up text-info" v-if="macro.position === 2"></i>
-                              <i class="fa fa-stack-1x fa-arrows-v text-success" v-if="macro.position === 3"></i>
-                            </i>
-                          </span>
+                      <draggable v-model='editing.value' element="ul" class='list-unstyled d-flex px-4 my-2 d-flex justify-content-center align-items-center flex-row flex-wrap'>
+
+                        <li class="macro-step flex-column justify-content-center align-items-center my-2" v-for="macro in editing.value" :key="macro.order">
+
+                          <div class="key d-flex">
+                            <div class="inner content">
+                              <div class="shift" v-if="macro.shift_key && macro.shifted">{{ macro.shift_key }}</div>
+                              <div v-if="macro.shift_key && macro.shifted">{{ macro.key }}</div>
+                              <div class='plain' v-if="!macro.shift_key">{{ macro.key }}</div>
+                            </div>
+
+                            <div class="inner hover">
+                              <i class="fa fa-fw fa-lg fa-times"></i>
+                            </div>
+
+                          </div>
+
+                          <div class="position mt-2">
+                            <span class="fa-stack fa-lg" @click="cycleMacroKeyPosition(macro)">
+                              <i class="fa fa-circle-thin fa-stack-2x"></i>
+                              <i class="fa fa-stack-1x fa-stack">
+                                <i class="fa fa-circle-thin fa-stack-2x fa-2x"></i>
+                                <i class="fa fa-stack-1x fa-long-arrow-down text-warning" v-if="macro.position === 1"></i>
+                                <i class="fa fa-stack-1x fa-long-arrow-up text-info" v-if="macro.position === 2"></i>
+                                <i class="fa fa-stack-1x fa-arrows-v text-success" v-if="macro.position === 3"></i>
+                              </i>
+                            </span>
+                          </div>
 
                         </li>
-                      </ul>
+
+                      </draggable>
+
                     </div>
                   </div>
+
+                  <!-- Keyboard Selector -->
+                  <!-- TODO - implement KEYBOARD selector -->
 
                 </div>
                 <!-- END MacroEditor -->
@@ -201,3 +223,79 @@ export default {
 </script>
 
 
+<style lang='sass' scoped>
+  $macro_size: 3rem
+
+  li.macro-step
+    display: flex
+    padding: 0 .25rem
+    min-height: 8rem
+
+    .key
+      height: $macro_size
+      width: $macro_size
+      border: .15rem solid #333333 // theme-color('secondary')
+      border-radius: .2rem
+      color: #999 // theme-color('light')
+      display: flex
+      font-size: .75rem
+      font-weight: 400
+      cursor: pointer
+
+      .inner
+        height: 100%
+        width: 100%
+        justify-content: center
+        align-items: center
+
+        &.content
+          display: flex
+          flex-direction: column
+
+          // Space button
+          &.space
+            color: rgba(255, 255, 255, 0)
+
+        &.hover
+          display: none
+
+    .position
+      display: flex
+      // opacity: 0
+
+    // li.macro-step HOVER state
+    &.hovered
+      // .position
+      //   opacity: 1
+
+      .key
+        // border: .15rem solid theme-color('secondary')
+
+        .inner.content
+          display: none
+        .inner.hover
+          display: flex
+          // color: lighten(theme-color('danger'), 25%)
+          color: red
+
+    // TODO - this should be adjusted to
+    &.drag-start
+      // opacity: 0
+      .key
+        .inner.content
+          display: flex
+        .inner.hover
+          display: none
+
+      .position
+        opacity: 0
+
+    &.ghost
+      .key
+        border-width: .3rem
+        border-color: #f3f3f3
+        border-style: dashed
+        // color: theme-color('warning')
+        color: orange
+
+</style>
