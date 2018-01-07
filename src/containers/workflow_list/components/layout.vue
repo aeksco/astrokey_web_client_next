3e9f537085769f0acea3e5e6614bf154f8ead388
Alexander Schwartzberg <aeksco@gmail.com>
2018-01-07 16:34:39 -0500

    Added WorkflowNew container & route

--- src/containers/workflow_list/components/layout.vue
+++ src/containers/workflow_list/components/layout.vue
@@ -6,7 +6,7 @@
 
     <div class='row'>
       <div v-for="workflow in collection" v-bind:key="workflow.serialNumber" class='col-lg-12 mt-2'>
-        <div class="card card-body bg-light">
+        <div class="card card-body bg-dark border-light">
           <div class="row">
 
             <div class="col-lg-8">
@@ -16,7 +16,7 @@
 
             <div class="col-lg-4 text-right">
               <!-- Open workflow -->
-              <a class='btn btn-sm btn-outline-dark' v-bind:href="'/#/workflows/' + workflow._id + '/edit'">
+              <a class='btn btn-sm btn-outline-light' v-bind:href="'/#/workflows/' + workflow._id + '/edit'">
                 <i class="fa fa-pencil mr-1"></i>
                 Edit
               </a>
