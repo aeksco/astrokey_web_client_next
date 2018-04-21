a00cd56f2a114e9688b2c2d8e3019cf20c2fde7f
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-21 15:35:50 -0400

    WorkflowEdit - tighten up state handling

--- src/store/workflow/actions.js
+++ src/store/workflow/actions.js
@@ -5,11 +5,15 @@ import WorkflowParser from './parser'
 // actions
 // functions that causes side effects and can involve asynchronous operations.
 const actions = {
-  // TODO - move to mutations?
-  serialize: ({ commit }, { workflow }) => WorkflowParser.serialize(workflow),
+  // serialize
+  serialize ({ commit }, { workflow }) {
+    WorkflowParser.serialize(workflow)
+  },
 
-  // TODO - move to mutations?
-  parse: ({ commit }, { data }) => WorkflowParser.parse(data)
+  // parse
+  parse ({ commit }, { data }) {
+    WorkflowParser.parse(data)
+  }
 }
 
 // // // //
