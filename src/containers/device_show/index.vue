c0c0f6bc449c563ed1fd0c387ea3225da6dbace6
Alexander Schwartzberg <aeksco@gmail.com>
2018-04-21 12:21:32 -0400

    DeviceShow stability improvements

--- src/containers/device_show/index.vue
+++ src/containers/device_show/index.vue
@@ -1,38 +1,61 @@
 
 <template>
-  <LayoutView :device="device" v-if="!!device" />
+  <div class="container-fluid">
+
+    <PageHeader title="Developer Interface" />
+
+    <div class="row">
+      <div class="col-lg-12">
+
+        <OpenDeviceButton :device="device" />
+        <CloseDeviceButton :device="device" />
+
+        <div class="card card-body bg-dark border-light text-light">
+          <div class="row">
+            <div class="col-lg-12">
+              <p class="card-text lead">
+                {{ device.productName }}
+                <small class='text-muted'>({{device.id}})</small>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+
 </template>
 
 <!-- // // // //  -->
 
 <script>
-import _ from 'lodash'
-import LayoutView from './components/layout.vue'
-import store from '@/store'
+import { mapGetters, mapActions } from 'vuex'
+import PageHeader from '@/components/PageHeader'
+import OpenDeviceButton from '@/components/OpenDeviceButton'
+import CloseDeviceButton from '@/components/CloseDeviceButton'
 
 export default {
   components: {
-    LayoutView
+    PageHeader,
+    OpenDeviceButton,
+    CloseDeviceButton
+  },
+  created () {
+    this.ensureDevice()
+  },
+  beforeDestroy () {
+    this.clearDevice()
   },
   metaInfo: {
-    title: 'Device - Show' // title is now "AstroKey - Device - Show"
+    title: 'Device - Developer Interface'
   },
-  props: ['id'],
-  // created () {
-  //   this.fetch()
-  // },
-  // methods: {
-  //   fetch () {
-  //     return store.dispatch('web_usb/getDevices')
-  //   }
-  // },
-  computed: {
-    device () {
-      // return _.find(store.getters['device/collection'], { serialNumber: this.id })
-      return _.find(store.getters['device/collection'], { id: this.id })
-    }
-  }
+  computed: mapGetters({
+    device: 'device/selectedDevice'
+  }),
+  methods: mapActions({
+    ensureDevice: 'device/ensureSelectedDevice',
+    clearDevice: 'device/clearSelectedDevice'
+  })
 }
 </script>
-
-
