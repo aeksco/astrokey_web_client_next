77a70d8d68d078ddfd61e3a041d570470171c414
Alexander Schwartzberg <aeksco@gmail.com>
2018-06-10 12:57:55 -0400

    ControlTransferForm - added input elements

--- src/containers/device_show/components/ControlTransferForm.vue
+++ src/containers/device_show/components/ControlTransferForm.vue
@@ -3,6 +3,12 @@
     <div class="col-lg-12">
       <p class="lead">Control Transfer Form</p>
       <pre class="bg-dark text-light">{{transferPayload}}</pre>
+
+      <input class='form-control' type='text' v-model="transferPayload.requestType"></input>
+      <input class='form-control' type='text' v-model="transferPayload.request"></input>
+      <input class='form-control' type='number' v-model="transferPayload.request"></input>
+      <input class='form-control' type='number' v-model="transferPayload.value"></input>
+      <input class='form-control' type='number' v-model="transferPayload.index"></input>
     </div>
   </div>
 </template>
