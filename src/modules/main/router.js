b55177c469887620ff53dfeb70ed145a5a37601d
Alexander Schwartzberg <aeksco@gmail.com>
2018-07-20 23:27:26 -0400

    Added about page

--- src/modules/main/router.js
+++ src/modules/main/router.js
@@ -1,12 +1,20 @@
 // Main Containers
 import MainHome from './views/home'
+import MainAbout from './views/about'
 
-const MainHomeRoute = {
+const HomeRoute = {
   path: '/',
   name: 'Home',
   component: MainHome
 }
 
+const AboutRoute = {
+  path: '/about',
+  name: 'About',
+  component: MainAbout
+}
+
 export default [
-  MainHomeRoute
+  HomeRoute,
+  AboutRoute
 ]
---
+++ src/modules/main/views/about/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <p class="lead">About</p>
+</template>
+
+<script>
+export default {}
+</script>
